19cf2aedb87f33d9fa9b06c57592bf79d508bd0a
Shaurya Joshi <43129037+shauryajsh@users.noreply.github.com>
2026-09-15 18:03:14 +1000

    Add BigQuery dataset resource, import existing dataset into state

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,8 @@ provider "google" {
   project = var.gcp_project_id
   region  = "us-central1"
 }
+
+resource "google_bigquery_dataset" "dbt_finops" {
+  dataset_id = "dbt_finops"
+  location   = "US"
+}
